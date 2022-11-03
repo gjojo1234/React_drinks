e426ad49e19e202619545234c062b836fdae6dbd
gjojo1234 <gensor1234@gmail.com>
2022-11-03 18:10:07 +0100

    about style p

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <>
       <h1>About</h1>
-      <p>
+      <p style={{ width: "35rem", textAlign: "center", margin: " 0 auto" }}>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus et
         voluptates atque, illo doloribus repellat praesentium voluptate sapiente
         enim minima possimus eos, consectetur quisquam aperiam sint harum aut
